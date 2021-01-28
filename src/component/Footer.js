d3db9da02a581751624f3c21fb77b24a79c276fa
radharukminiY <rukku558@gmail.com>
2021-01-28 20:44:10 +0530

    commit 2

---
+++ src/component/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer>
+            <div>
+                <hr/>
+                &copy; News-Forum-2021
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
