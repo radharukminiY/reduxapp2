5e57c269e3d6e1c7b7b3923c0d9946b241aa5f41
radharukminiY <rukku558@gmail.com>
2021-01-28 22:13:37 +0530

    commit 4

---
+++ src/reducers/main_reducer.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import article from './article_reducer';
+import gallery from './gallery_reducer';
+
+const rootReducer = combineReducers({
+
+})
+export default rootReducer;
