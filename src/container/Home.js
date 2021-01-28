77ec01ffa647f92d215d99114e442b1acfbf6a17
radharukminiY <rukku558@gmail.com>
2021-01-28 22:48:03 +0530

    commit 5

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,33 +1,33 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
-import {latestNews,articleNews,gallertNews} from '../actions/actionfile';
+import {latestNews,articleNews,galleryNews} from '../actions/actionfile';
 import LatestDisplay from '../component/Home/LatestDisplay';
 import ArticleDisplay from '../component/Home/ArticleDisplay';
 import GalleryDisplay from '../component/Home/GalleryDisplay';
 
 class Home extends Component{
-  componentDidMount(){
-    this.props.dispatch(latestNews())
-    this.props.dispatch(articleNews())
-    this.props.dispatch(gallertNews())
+    componentDidMount(){
+        this.props.dispatch(latestNews())
+        this.props.dispatch(articleNews())
+        this.props.dispatch(galleryNews())
+    }
 
-  }
-  render(){
-    return(
-      <div>
-      <LatestDisplay ldata={this.props.datalist.latestNews}/>
-      <ArticleDisplay adata={this.props.datalist.articleNews}/>
-      <GalleryDisplay gdata={this.props.gallerylist.gallertNews}/>
-      </div>
-    )
-  }
+    render(){
+        return(
+            <div>
+               <LatestDisplay ldata={this.props.datalist.latestNews}/>
+               <ArticleDisplay adata={this.props.datalist.articleNews}/>
+               <GalleryDisplay gdata={this.props.gallist.galleryNews}/>
+            </div>
+        )
+    }
 }
 
 function mapStateToProps(state){
-  return{
-    datalist:state.article,
-    gallerylist:state.gallery
-  }
+    return{
+        datalist:state.article,
+        gallist:state.gallery
+    }
 }
 
 export default connect(mapStateToProps)(Home)
--- src/reducers/main_reducer.js
+++ src/reducers/main_reducer.js
@@ -3,6 +3,7 @@ import article from './article_reducer';
 import gallery from './gallery_reducer';
 
 const rootReducer = combineReducers({
-
+  article,
+   gallery
 })
 export default rootReducer;
